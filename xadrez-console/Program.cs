fe93a8898ff14df0669e7b960f35cb5b3b8b09e0
André Bueno <andrebueno99@gmail.com>
2023-05-01 18:52:45 -0300

    Criando classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using xadrez_console.tabuleiro;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Digite a posicao da linha");
+        int psL = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite a posicao da coluna");
+        int psC = int.Parse(Console.ReadLine());
+
+        Posicao ps = new Posicao(psL, psC);
+
+        Console.WriteLine("Posicoes: l = "+  ps.linha + ", c = " + ps.coluna);
+    }
+}
